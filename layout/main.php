81dc062b7fbf4327388a3fb9413e28def839c826
arieeefajar <ariefajar739@gmail.com>
2023-10-17 16:26:37 +0700

    tidy up folders

--- layout/main.php
+++ layout/main.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 
-// var_dump($_SESSION["idUser"]);
 if (!isset($_SESSION["idUser"])) {
     echo '<script>alert("Harap login terlebih dahulu");
-    window.location.href=("index.php")</script>';
+    window.location.href=("../auth/index.php")</script>';
 }
 ?>
 <!DOCTYPE html>
@@ -27,15 +26,15 @@ if (!isset($_SESSION["idUser"])) {
     </title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= 'http://localhost/SI/tokoFarda/'; ?>css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
     <!-- sweetalert -->
     <link rel="stylesheet" href="<?= 'http://localhost/SI/tokoFarda/'; ?>sweetalert2/sweetalert2.min.css">
@@ -53,9 +52,9 @@ if (!isset($_SESSION["idUser"])) {
                 <!-- Content Wrapper -->
                 <?php
                 if (empty($_GET['page'])) {
-                    include 'dashboard.php';
+                    include '../admin/dashboard.php';
                 } elseif ($_GET['page'] = 'dataUser') {
-                    include 'dataUser.php';
+                    include '../admin/dataUser.php';
                 }
                 ?>
                 <!-- End of Content Wrapper -->
@@ -83,7 +82,7 @@ if (!isset($_SESSION["idUser"])) {
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
+                    <a class="btn btn-primary" href="../auth/logout.php">Logout</a>
                 </div>
             </div>
         </div>
@@ -91,39 +90,39 @@ if (!isset($_SESSION["idUser"])) {
 
     <!-- Bootstrap core JavaScript-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/jquery/jquery.min.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/chart.js/Chart.min.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
     <!-- <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script> -->
-    <script src="js/demo/datatables-demo.js"></script>
+    <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>js/demo/datatables-demo.js"></script>
 
     <!-- sweetalert2 -->
     <script src="<?= 'http://localhost/SI/tokoFarda/'; ?>sweetalert2/sweetalert2.min.js"></script>
 
-    <!-- <script>
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Your work has been saved',
-            showConfirmButton: false,
-            timer: 1500
-        })
-    </script> -->
-
     <script>
+        <?php if (isset($_SESSION['statusLogin'])) : ?>
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: '<?= $_SESSION['statusLogin']; ?>',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        <?php unset($_SESSION['statusLogin']);
+        endif; ?>
         <?php if (isset($_SESSION['success'])) : ?>
             Swal.fire({
                 position: 'center',
