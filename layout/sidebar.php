edfeb9215f8c1f2d98a977f4f8418124a3c4295f
arieeefajar <ariefajar739@gmail.com>
2023-12-17 22:02:03 +0700

    fixed sidebar

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -125,6 +125,21 @@
             </div>
         </li>
 
+        <!-- History menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#historiMenu" aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-clock"></i>
+                <span>Histori</span>
+            </a>
+            <div id="historiMenu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Histori Menu:</h6>
+                    <a class="collapse-item" href="?page=historiJual">Jual</a>
+                    <a class="collapse-item" href="?page=historiBeli">Beli</a>
+                </div>
+            </div>
+        </li>
+
         <!-- Laporan menu -->
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#laporanMenu" aria-expanded="true" aria-controls="collapseTwo">
